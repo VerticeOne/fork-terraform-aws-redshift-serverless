60138275a9f694cd2ed45e06cabf059c25cb47d4
Horst Rohrweck <horst.rohrweck@vertice.one>
2025-07-21 05:48:33 +0200

    NAVY-103: change terraform version constraints from 1.11 to 1.12

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@
 ##-----------------------------------------------------------------------------
 # Terraform
 terraform {
-  required_version = "~> 1.11.0"
+  required_version = "~> 1.12.0"
   required_providers {
     aws = {
       source                = "hashicorp/aws"
