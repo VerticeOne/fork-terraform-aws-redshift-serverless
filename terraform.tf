1ee8f98090b676182ed025bda4452051ed6004b0
Horst Rohrweck <horst.rohrweck@vertice.one>
2025-09-10 13:18:09 +0200

    NAVY-103: Fix some minor issues, adding pre-commit hooks

--- terraform.tf
+++ terraform.tf
@@ -6,5 +6,13 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 5.0, < 6.0"
     }
+    random = {
+      source = "hashicorp/random"
+      version = "3.7.2"
+    }
+    null = {
+      source = "hashicorp/null"
+      version = "3.2.4"
+    }
   }
 }
